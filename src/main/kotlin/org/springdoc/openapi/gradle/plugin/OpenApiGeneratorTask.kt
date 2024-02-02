5fedee8e49b849e64683b6fb65acd85f4c5c7b07
Jonathan Kaeser <jonathan.kaeser@adnovum.ch>
2024-02-02 16:21:48 +0100

    adds @UntrackedTask annotation on OpenApiGeneratorTask

--- src/main/kotlin/org/springdoc/openapi/gradle/plugin/OpenApiGeneratorTask.kt
+++ src/main/kotlin/org/springdoc/openapi/gradle/plugin/OpenApiGeneratorTask.kt
@@ -15,10 +15,7 @@ import org.gradle.api.GradleException
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.provider.MapProperty
 import org.gradle.api.provider.Property
-import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.Internal
-import org.gradle.api.tasks.OutputDirectory
-import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.*
 import java.net.ConnectException
 import java.net.HttpURLConnection
 import java.net.URL
@@ -27,6 +24,7 @@ import java.time.temporal.ChronoUnit.SECONDS
 
 private const val MAX_HTTP_STATUS_CODE = 299
 
+@UntrackedTask(because="never up to date")
 open class OpenApiGeneratorTask : DefaultTask() {
 	@get:Input
 	val apiDocsUrl: Property<String> = project.objects.property(String::class.java)
